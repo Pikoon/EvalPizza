084b55171f7632fe66de22609153c7da60903606
gaetan <gaetan.cren@hotmail.fr>
2024-06-19 13:27:33 +0200

    Initial commit

---
+++ src/main/java/com/example/evalpizza/EvalPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.example.evalpizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalPizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvalPizzaApplication.class, args);
+    }
+
+}
